5d2d77ba9d0ae4af893cb79ad737d1d3d666fb8c
IsaacB715 <111525800+IsaacB715@users.noreply.github.com>
2022-09-18 11:30:15 -0400

    Update pytech_update.py

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -1,1 +1,21 @@
+from pymongo import MongoClient
 
+def display_student(student):
+    print("Student ID: ", student["student_id"])
+    print("First Name: ", student["first_name"])
+    print("Last Name: ", student["last_name"])
+    print()
+
+url = "mongodb+srv://admin:admin@cluster0.vppafvn.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client.pytech
+print(db.list_collection_names)
+students = db.get_collection("students")
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+for student in students.find(): 
+    display_student(student)
+result = students.update_one({"student_id": "1007"}, {"$set": {"last_name": "Smith"}})
+
+print("-- DISPLAYING STUDENT DOCUMENT 1007 --")
+student = students.find_one({"student_id": "1007"})
+display_student(student)
